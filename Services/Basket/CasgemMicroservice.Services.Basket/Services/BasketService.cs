281b4d7105d4f2a2846b3274ac9733bacd813fb4
zehrasbr <zehraisbr@gmail.com>
2023-08-09 16:44:47 +0300

    update8

---
+++ Services/Basket/CasgemMicroservice.Services.Basket/Services/BasketService.cs
@@ -0,0 +1,39 @@
+﻿using CasgemMicroservice.Services.Basket.Dtos;
+using CasgemMicroservice.Shared.Dtos;
+using System.Text.Json;
+
+namespace CasgemMicroservice.Services.Basket.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+
+        public async Task<Response<bool>> DeleteBasket(string userID)
+        {
+            var status = await _redisService.GetDb().KeyDeleteAsync(userID);
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Sepet Bulunamadı", 404);
+        }
+
+        public async Task<Response<BasketDto>> GetBasket(string userID)
+        {
+            var existBasket = await _redisService.GetDb().StringGetAsync(userID);
+            if (string.IsNullOrEmpty(existBasket))
+            {
+                return Response<BasketDto>.Fail("Sepet Bulunamadı", 404);
+            }
+            return Response<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200);
+        }
+
+        public async Task<Response<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDb().StringSetAsync(basketDto.UserID, JsonSerializer.Serialize(basketDto));
+            return status ? Response<bool>.Success(204) : Response<bool>.Fail("Sepet Güncelleme veya Ekleme yapılamadı", 500);
+
+        }
+    }
+}
---
+++ Services/Basket/CasgemMicroservice.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using CasgemMicroservice.Services.Basket.Dtos;
+using CasgemMicroservice.Shared.Dtos;
+
+namespace CasgemMicroservice.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        Task<Response<BasketDto>> GetBasket(string UserID);
+        Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<Response<bool>> DeleteBasket(string UserID);
+    }
+}
