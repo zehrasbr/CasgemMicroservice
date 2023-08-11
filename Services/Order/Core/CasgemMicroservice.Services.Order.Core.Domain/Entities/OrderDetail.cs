2147cdee1a8f17182e10542e74a422da51fd6e86
zehrasbr <zehraisbr@gmail.com>
2023-08-11 14:24:10 +0300

    update11

---
+++ Services/Order/Core/CasgemMicroservice.Services.Order.Core.Domain/Entities/OrderDetail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasgemMicroservice.Services.Order.Core.Domain.Entities
+{
+    public class OrderDetail
+    {
+        public int OrderDetailID { get; set; }
+        public string UserID { get; set; }
+    }
+}
