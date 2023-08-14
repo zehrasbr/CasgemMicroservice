27384d30363d9f1472182049b5d56d2e1f8ef014
zehrasbr <zehraisbr@gmail.com>
2023-08-14 12:13:46 +0300

    update19

---
+++ Services/Presentation/CasgemMicroservice.Services.Order.Presentation.Api/Controllers/OrderDetailsController.cs
@@ -0,0 +1,57 @@
+﻿using CasgemMicroservice.Services.Order.Core.Application.Features.CQRS.Commands;
+using CasgemMicroservice.Services.Order.Core.Application.Features.CQRS.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemMicroservice.Services.Order.Presentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderDetailsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderDetailsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> OrderDetailList()
+        {
+            var values = _mediator.Send(new GetAllOrderDetailQueryRequest());
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> OrderDetailGetById(int id)
+        {
+            var value = _mediator.Send(new GetByIdOrderDetailQueryRequest(id));
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> OrderDetailCreate(CreateOrderDetailCommandRequest createOrderDetailCommandRequest)
+        {
+            await _mediator.Send(createOrderDetailCommandRequest);
+            return Ok("Sipariş Detayı Eklendi");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> OrderDetailUpdate(UpdateOrderDetailCommandRequest updateOrderDetailCommandRequest)
+        {
+            await _mediator.Send(updateOrderDetailCommandRequest);
+            return Ok("Sipariş Detayı Güncellendi");
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> OrderDetailDelete(int id)
+        {
+            await _mediator.Send(new RemoveOrderDetailCommandRequest(id));
+            return Ok("Sipariş Detayı Silindi");
+
+        }
+    }
+}
+
---
+++ Services/Presentation/CasgemMicroservice.Services.Order.Presentation.Api/Controllers/OrderingController.cs
@@ -0,0 +1,50 @@
+﻿using CasgemMicroservice.Services.Order.Core.Application.Features.CQRS.Commands;
+using CasgemMicroservice.Services.Order.Core.Application.Features.CQRS.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemMicroservice.Services.Order.Presentation.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderingController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public OrderingController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async Task<IActionResult> OrderingList()
+        {
+            var values = await _mediator.Send(new GetAllOrderingQueryRequest());
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> OrderingGetById(int id)
+        {
+            var value = await _mediator.Send(new GetByIdOrderingQueryRequest(id));
+            return Ok(value);
+        }
+        [HttpPost]
+        public async Task<IActionResult> OrderingCreate(CreateOrderingCommandRequest createOrderingCommandRequest)
+        {
+            await _mediator.Send(createOrderingCommandRequest);
+            return Ok("Sipariş Eklendi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> OrderingUpdate(UpdateOrderingCommandRequest updateOrderingCommandRequest)
+        {
+            await _mediator.Send(updateOrderingCommandRequest);
+            return Ok("Sipariş Güncellendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> OrderingDelete(RemoveOrderingCommandRequest removeOrderingCommandRequest)
+        {
+            await _mediator.Send(removeOrderingCommandRequest);
+            return Ok("Sipariş Silindi");
+        }
+    }
+}
