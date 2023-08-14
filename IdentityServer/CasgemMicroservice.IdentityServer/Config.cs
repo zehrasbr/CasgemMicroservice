b7550b2a1f9f87862b4fe1140cec28fa3ad33367
zehrasbr <zehraisbr@gmail.com>
2023-08-14 14:40:23 +0300

    update20

--- IdentityServer/CasgemMicroservice.IdentityServer/Config.cs
+++ IdentityServer/CasgemMicroservice.IdentityServer/Config.cs
@@ -16,6 +16,7 @@ namespace CasgemMicroservice.IdentityServer
                 new ApiResource("resource_photostock"){Scopes={"photostock_fullpermission"}},
                     new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
                           new ApiResource("resource_discount"){Scopes={"discount_fullpermission"}},
+                              new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
 				        new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
         public static IEnumerable<IdentityResource> IdentityResources =>
@@ -33,6 +34,7 @@ namespace CasgemMicroservice.IdentityServer
                 new ApiScope("photostock_fullpermission","Fotoğraf İşlemleri İçin Tam Erişim"),
                 new ApiScope("basket_fullpermission","Sepet İçin Tam Erişim"),
                 new ApiScope("discount_fullpermission","İndirim İçin Tam Erişim"),
+                new ApiScope("order_fullpermission","Sipariş İçin Tam Erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
             };
 
@@ -56,7 +58,7 @@ namespace CasgemMicroservice.IdentityServer
                     ClientSecrets = { new Secret("secret".Sha256()) },
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                     AllowOfflineAccess = true,
-                    AllowedScopes = { "catalog_fullpermission", "basket_fullpermission","photostock_fullpermission", "discount_fullpermission",
+                    AllowedScopes = { "catalog_fullpermission", "basket_fullpermission","photostock_fullpermission", "discount_fullpermission","order_fullpermission",
                         IdentityServerConstants.LocalApi.ScopeName,
                         IdentityServerConstants.StandardScopes.Email,
                         IdentityServerConstants.StandardScopes.OpenId,
--- Services/Presentation/CasgemMicroservice.Services.Order.Presentation.Api/Program.cs
+++ Services/Presentation/CasgemMicroservice.Services.Order.Presentation.Api/Program.cs
@@ -2,13 +2,31 @@ using CasgemMicroservice.Services.Order.Core.Application;
 using CasgemMicroservice.Services.Order.Core.Application.Interfaces;
 using CasgemMicroservice.Services.Order.Infrastructure.Persistance.Context;
 using CasgemMicroservice.Services.Order.Infrastructure.Persistance.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
+
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+    opt.Authority = builder.Configuration["IdentityServerUrl"];
+    opt.Audience = "resource_order";
+    opt.RequireHttpsMetadata = false;
+});
+builder.Services.AddControllers(opt=>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,6 +45,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
