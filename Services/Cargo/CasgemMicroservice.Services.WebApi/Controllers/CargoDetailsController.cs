5c9118c0cb908206b14b9a8afc9cf6c53f8d68a5
zehrasbr <zehraisbr@gmail.com>
2023-08-15 09:50:55 +0300

    update24

---
+++ Services/Cargo/CasgemMicroservice.Services.WebApi/Controllers/CargoDetailsController.cs
@@ -0,0 +1,49 @@
+﻿using CasgemMicroservice.Services.Cargo.BusinessLayer.Abstract;
+using CasgemMicroservice.Services.Cargo.EntityLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemMicroservice.Services.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoDetailsController : ControllerBase
+    {
+        private readonly ICargoDetailService _cargoDetailService;
+
+        public CargoDetailsController(ICargoDetailService cargoDetailService)
+        {
+            _cargoDetailService = cargoDetailService;
+        }
+        [HttpGet]
+        public IActionResult CargoDetailList()
+        {
+            var values = _cargoDetailService.GetAll();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult CargoDetailGet(int id)
+        {
+            var value = _cargoDetailService.GetById(id);
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CargoDetailCreate(CargoDetail cargoDetail)
+        {
+            _cargoDetailService.Insert(cargoDetail);
+            return Ok("Kargo Detayı Eklendi");
+        }
+        [HttpPut]
+        public IActionResult CargoDetailUpdate(CargoDetail cargoDetail)
+        {
+             _cargoDetailService.Update(cargoDetail);
+            return Ok("Kargo Detayı Güncellendi");
+        }
+        [HttpDelete]
+        public IActionResult CargoDetailDelete(CargoDetail cargoDetail)
+        {
+            _cargoDetailService.Delete(cargoDetail);
+            return Ok("Kargo Detayı Silindi");
+        }
+    }
+}
---
+++ Services/Cargo/CasgemMicroservice.Services.WebApi/Controllers/CargoStatesController.cs
@@ -0,0 +1,50 @@
+﻿using CasgemMicroservice.Services.Cargo.BusinessLayer.Abstract;
+using CasgemMicroservice.Services.Cargo.EntityLayer.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasgemMicroservice.Services.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CargoStatesController : ControllerBase
+    {
+        private readonly ICargoStateService _cargoStateService;
+
+        public CargoStatesController(ICargoStateService cargoStateService)
+        {
+            _cargoStateService = cargoStateService;
+        }
+
+        [HttpGet]
+        public IActionResult CargoStateList()
+        {
+            var values = _cargoStateService.GetAll();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult CargoStateGet(int id)
+        {
+            var value = _cargoStateService.GetById(id);
+            return Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CargoStateCreate(CargoState cargoState)
+        {
+            _cargoStateService.Insert(cargoState);
+            return Ok("Kargo Durumu Eklendi");
+        }
+        [HttpPut]
+        public IActionResult CargoStateUpdate(CargoState cargoState)
+        {
+            _cargoStateService.Update(cargoState);
+            return Ok("Kargo Durumu Güncellendi");
+        }
+        [HttpDelete]
+        public IActionResult CargoStateDelete(CargoState cargoState)
+        {
+            _cargoStateService.Delete(cargoState);
+            return Ok("Kargo Durumu Silindi");
+        }
+    }
+}
